494ffe1056a5a3f5512eba9cf82fac9dd055c54c
Jaime <jhernandez@lsv-tech.com>
2019-07-26 16:49:09 -0500

    little changes

--- anime_importer_app/models.py
+++ anime_importer_app/models.py
@@ -6,6 +6,8 @@ from .utils.file_validations import validator
 class AnimeFile(models.Model):
     file = models.FileField(validators=[validator], upload_to='files/%Y_%m_%d/%H:%M:%S')
 
+    def __str__(self):
+        return self.file
 
 class Anime(models.Model):
     anime_id = models.IntegerField(primary_key=True)
@@ -16,8 +18,14 @@ class Anime(models.Model):
     rating = models.FloatField()
     members = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class AnimeRating(models.Model):
     user_id = models.IntegerField()
     anime_fk = models.ForeignKey('Anime', on_delete=models.CASCADE, null=True)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return self.anime_fk.name
